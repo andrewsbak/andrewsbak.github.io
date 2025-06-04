659798e31686b6b6457550a6455c987c4a657e5e
Andrew Bak <drewbak10@gmail.com>
2025-06-04 15:51:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
 window.addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(ScrollTrigger);
 
-  const chars = document.querySelectorAll("#hero-title .char");
-
   gsap.fromTo(
-    chars,
+    "#hero-title",
     {
-      willChange: "opacity, transform",
-      opacity: 0,
-      yPercent: 120,
-      scaleY: 2.3,
-      scaleX: 0.7,
-      transformOrigin: "50% 0%",
+      yPercent: 30,
+      opacity: 0.6,
     },
     {
-      opacity: 1,
       yPercent: 0,
-      scaleY: 1,
-      scaleX: 1,
-      duration: 1,
-      ease: "back.inOut(2)",
-      stagger: 0.03,
+      opacity: 1,
+      ease: "power3.out",
       scrollTrigger: {
         trigger: "#hero-title",
-        start: "center bottom+=50%",
-        end: "bottom bottom-=40%",
-        scrub: true
+        start: "top center+=100",
+        end: "bottom top-=100",
+        scrub: true,
       }
     }
   );
